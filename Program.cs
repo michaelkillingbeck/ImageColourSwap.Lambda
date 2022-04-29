cba8b88c49d589d2c2a886502aa58d5dec6266e9
Michael Killingbeck <8310367+michaelkillingbeck@users.noreply.github.com>
2022-04-29 13:27:11 +0100

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.Lambda.Core;
+
+namespace ImageColourSwap.Lambda
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+        }
+
+        [LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+        public string Handler(Stream request)
+        {
+            ImageColourSwapCore core = new ImageColourSwapCore();
+
+            return "Testing...testing 1 2 3";
+        }
+    }
+}
